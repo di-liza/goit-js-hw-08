87bb6b0880c713395408b589f3a713793356ea4e
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-03-12 22:17:05 +0200

    udate 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,37 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const inputMailEl = document.querySelector('.feedback-form input');
+const inputEmailEl = document.querySelector('.feedback-form input');
 const textareaEl = document.querySelector('.feedback-form textarea');
 const formData = {};
 
-formEl.addEventListener('input', onFormInput);
+checkLocalStorageOnLoad();
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  e.target.reset();
+}
 
 function onFormInput(e) {
-  formData[e.target.name] = e.terget.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  formData[e.target.name] = e.target.value;
+  const strFormData = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', strFormData);
+}
+
+function checkLocalStorageOnLoad() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const parseFormValue = JSON.parse(savedMessage);
+
+  if (savedMessage) {
+    inputEmailEl.value =
+      parseFormValue.email !== undefined ? parseFormValue.email : '';
+
+    textareaEl.value =
+      parseFormValue.message !== undefined ? parseFormValue.message : '';
+  }
 }
