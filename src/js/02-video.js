dc9a9f7d82436024d074a8cb5b442f472fd4fae6
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-03-12 21:57:54 +0200

    change tast 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,24 +2,24 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframeEl = document.querySelector('#vimeo-player');
-
 const player = new Player(iframeEl);
 
+player.on('timeupdate', throttle(onIframePlay, 1000));
+
 let currentTime = null;
-loadSavedVideoTime();
+onPageReboot();
 
-player.on('timeupdate', throttle(saveVideoTime, 1000));
-
-function saveVideoTime(data) {
+function onIframePlay(data) {
   localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify(data.seconds)
   );
 }
 
-function loadSavedVideoTime() {
+function onPageReboot() {
   currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-  if (currentTime !== null) {
-    player.setCurrentTime(parseInt(currentTime));
+
+  if (currentTime !== 0) {
+    player.setCurrentTime(currentTime);
   }
 }
